614d673f04a6fb2bb26dff457b0ed0ac900bd9c9
DongHyunShin <personarpg7501@gmail.com>
2023-12-26 20:47:16 +0900

    check : 11페이지 매김부터 다시 봐야함

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,18 +1,15 @@
 'use client';
-
+ 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
 
-export default function Search({ placeholder }: {
-  placeholder: string
-}) {
+export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
-
-  // Inside the Search Component...
   const handleSearch = useDebouncedCallback((term) => {
+
     console.log(`Searching... ${term}`);
 
     const params = new URLSearchParams(searchParams);
@@ -22,9 +19,8 @@ export default function Search({ placeholder }: {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }, 300);
-
-
+    },300)
+ 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -35,6 +31,7 @@ export default function Search({ placeholder }: {
         placeholder={placeholder}
         onChange={(e) => {
           handleSearch(e.target.value);
+          
         }}
         defaultValue={searchParams.get('query')?.toString()}
 
